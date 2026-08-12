53ec6f969995d38fc0e214ff9a88e890a5026935
TSi <tl.upi@ya.ru>
2026-08-12 22:08:11 +0700

    Тесты, Sprint 5

--- src/test/java/com/example/PredatorTest.java
+++ src/test/java/com/example/PredatorTest.java
@@ -2,11 +2,8 @@ package com.example;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 
